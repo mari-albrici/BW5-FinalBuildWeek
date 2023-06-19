08913c3883e931f2abd056e6e861297b144c8d88
Nestor162 <nestorcicardini@gmail.com>
2023-06-19 15:55:35 +0200

    Create Address Repository

---
+++ EPIC_ENERGY_ERVICES/src/main/java/team5/EPIC_ENERGY/_ERVICES/address/AddressRepository.java
@@ -0,0 +1,9 @@
+package team5.EPIC_ENERGY._ERVICES.address;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+
+}
