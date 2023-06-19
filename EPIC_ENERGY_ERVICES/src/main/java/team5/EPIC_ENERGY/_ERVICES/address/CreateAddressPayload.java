003549a34beea81487594e449fe00a04c19bbc3e
Nestor162 <nestorcicardini@gmail.com>
2023-06-19 15:57:17 +0200

    Add Getters

--- EPIC_ENERGY_ERVICES/src/main/java/team5/EPIC_ENERGY/_ERVICES/address/CreateAddressPayload.java
+++ EPIC_ENERGY_ERVICES/src/main/java/team5/EPIC_ENERGY/_ERVICES/address/CreateAddressPayload.java
@@ -2,7 +2,9 @@ package team5.EPIC_ENERGY._ERVICES.address;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
 
+@Getter
 public class CreateAddressPayload {
 	@NotNull(message = "Street is required")
 	@Size(min = 3, max = 30, message = "Street must be between 3 and 30 characters")
