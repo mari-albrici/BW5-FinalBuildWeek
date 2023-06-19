3f56e3c6b47dd640a56ca56fceeaef79fb3eb3f4
Marianna <marianna.albrici@gmail.com>
2023-06-19 10:16:04 +0200

    First commit

---
+++ EPIC_ENERGY_ERVICES/src/main/java/team5/EPIC_ENERGY/_ERVICES/EpicEnergyErvicesApplication.java
@@ -0,0 +1,13 @@
+package team5.EPIC_ENERGY._ERVICES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicEnergyErvicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicEnergyErvicesApplication.class, args);
+	}
+
+}
---
+++ EPIC_ENERGY_ERVICES/src/test/java/team5/EPIC_ENERGY/_ERVICES/EpicEnergyErvicesApplicationTests.java
@@ -0,0 +1,13 @@
+package team5.EPIC_ENERGY._ERVICES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyErvicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
