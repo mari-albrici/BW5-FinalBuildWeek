3f7f4442037e194d9a9d6b2f2cc6ddf31615d8ac
Nestor162 <nestorcicardini@gmail.com>
2023-06-19 15:55:16 +0200

    Create payload with validation

---
+++ EPIC_ENERGY_ERVICES/src/main/java/team5/EPIC_ENERGY/_ERVICES/address/CreateAddressPayload.java
@@ -0,0 +1,20 @@
+package team5.EPIC_ENERGY._ERVICES.address;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class CreateAddressPayload {
+	@NotNull(message = "Street is required")
+	@Size(min = 3, max = 30, message = "Street must be between 3 and 30 characters")
+	String street;
+
+	@NotNull(message = "Building number is required")
+	String buildingNumber;
+
+	@NotNull(message = "City is required")
+	String city;
+
+	@NotNull(message = "ZIP Code is required")
+	@Size(min = 5, max = 5, message = "ZIP code must have 5 digits")
+	String zipCode;
+}
