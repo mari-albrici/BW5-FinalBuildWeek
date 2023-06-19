52f9860af997b081af11196ee2b08adfca6027f0
Nestor162 <nestorcicardini@gmail.com>
2023-06-19 15:20:40 +0200

    Custom find method on repository (findByProvinceName

--- EPIC_ENERGY_ERVICES/src/main/java/team5/EPIC_ENERGY/_ERVICES/municipality/MunicipalityRepository.java
+++ EPIC_ENERGY_ERVICES/src/main/java/team5/EPIC_ENERGY/_ERVICES/municipality/MunicipalityRepository.java
@@ -1,8 +1,12 @@
 package team5.EPIC_ENERGY._ERVICES.municipality;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MunicipalityRepository
 		extends JpaRepository<Municipality, Long> {
+
+	Optional<Municipality> findByProvinceName(String province);
 
 }
