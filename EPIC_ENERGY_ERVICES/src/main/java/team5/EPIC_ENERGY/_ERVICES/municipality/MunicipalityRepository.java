5036c3b342bfb41adb7b3ca49d931cd722eaece3
Nestor162 <nestorcicardini@gmail.com>
2023-06-19 12:23:27 +0200

    Create Municipality Repository

---
+++ EPIC_ENERGY_ERVICES/src/main/java/team5/EPIC_ENERGY/_ERVICES/municipality/MunicipalityRepository.java
@@ -0,0 +1,8 @@
+package team5.EPIC_ENERGY._ERVICES.municipality;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MunicipalityRepository
+		extends JpaRepository<Municipality, Long> {
+
+}
