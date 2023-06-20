13aa4faeb04b5c4af9b8c63dfc90fc4a875ac982
Nestor162 <nestorcicardini@gmail.com>
2023-06-20 17:43:53 +0200

    Add address from db in Faker

--- EPIC_ENERGY_SERVICES/src/main/java/team5/EPIC_ENERGY_SERVICES/customers/CustomerRunner.java
+++ EPIC_ENERGY_SERVICES/src/main/java/team5/EPIC_ENERGY_SERVICES/customers/CustomerRunner.java
@@ -1,46 +1,67 @@
 package team5.EPIC_ENERGY_SERVICES.customers;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.Locale;
 import com.github.javafaker.Faker;
+
+import team5.EPIC_ENERGY_SERVICES.address.AddressRepository;
+import team5.EPIC_ENERGY_SERVICES.address.AddressService;
+import team5.EPIC_ENERGY_SERVICES.municipality.MunicipalityService;
 
 @Component
 public class CustomerRunner implements CommandLineRunner {
 
-    @Autowired
-    private CustomerService customerService;
+	@Autowired
+	private CustomerService customerService;
 
-    @Override
-    public void run(String... args) throws Exception {
+	@Autowired
+	AddressService addressService;
 
-        Faker faker = new Faker(new Locale("it"));
+	@Autowired
+	MunicipalityService municipalityService;
 
-        for (int i = 0; i < 10; i++)
-            try {
-                Customer customer = new Customer();
-                customer.setBusinessName(faker.funnyName().name());
-                customer.setAdded(LocalDate.of(2023, 01, 01));
-                customer.setLastContact(customer.getAdded().plusDays(42));
-                customer.setCustomerType(BusinessType.SAS);
-                customer.setEmail(faker.internet().emailAddress());
-                customer.setAnnualTurnover(BigDecimal.valueOf(faker.number().randomNumber()));
-                customer.setContactEmail(faker.internet().emailAddress());
-                customer.setContactLastname(faker.name().lastName());
-                customer.setContactName(faker.name().firstName());
-                customer.setContactPhone(faker.phoneNumber().phoneNumber());
-                customer.setPec(faker.internet().emailAddress());
-                customer.setPhoneNo(faker.phoneNumber().phoneNumber());
-                customer.setLegalAddress(faker.address().fullAddress());
-                customer.setVATNumber(String.valueOf(faker.number().numberBetween(111111111, 999999999)));
-                customerService.create(customer);
-            } catch (Exception e) {
-                System.out.println(e);
-            }
+	@Autowired
+	AddressRepository addressRepo;
 
-    }
+	@Override
+	public void run(String... args) throws Exception {
+
+		Faker faker = new Faker(new Locale("it"));
+
+		for (int i = 0; i < 10; i++)
+			try {
+				Customer customer = new Customer();
+				customer.setBusinessName(faker.funnyName().name());
+				customer.setAdded(LocalDate.of(2023, 01, 01));
+				customer.setLastContact(customer.getAdded().plusDays(42));
+				customer.setCustomerType(BusinessType.SAS);
+				customer.setEmail(faker.internet().emailAddress());
+				customer.setAnnualTurnover(
+						BigDecimal.valueOf(faker.number().randomNumber()));
+				customer.setContactEmail(faker.internet().emailAddress());
+				customer.setContactLastname(faker.name().lastName());
+				customer.setContactName(faker.name().firstName());
+				customer.setContactPhone(faker.phoneNumber().phoneNumber());
+				customer.setPec(faker.internet().emailAddress());
+				customer.setPhoneNo(faker.phoneNumber().phoneNumber());
+//				customer.setLegalAddress(faker.address().fullAddress());
+//				customer.setLegalAddress(addressRepo
+//						.findById(UUID.fromString(
+//								"b3797739-2a43-454a-8495-9766c9b05bcf"))
+//						.orElseThrow(() -> new NotFoundException(
+//								"Address not found")));
+				customer.setVATNumber(String.valueOf(
+						faker.number().numberBetween(111111111, 999999999)));
+				customerService.create(customer);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+
+	}
 }
