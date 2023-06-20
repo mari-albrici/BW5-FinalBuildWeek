36f1d93cd3cab53afc8e12e1decb25edfcb7522e
Nestor162 <nestorcicardini@gmail.com>
2023-06-20 17:54:28 +0200

    Add OneToOne relationship with Address

--- EPIC_ENERGY_SERVICES/src/main/java/team5/EPIC_ENERGY_SERVICES/customers/Customer.java
+++ EPIC_ENERGY_SERVICES/src/main/java/team5/EPIC_ENERGY_SERVICES/customers/Customer.java
@@ -1,47 +1,61 @@
 package team5.EPIC_ENERGY_SERVICES.customers;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import team5.EPIC_ENERGY_SERVICES.address.Address;
+
 @Entity
-@Table(name= "customers")
+@Table(name = "customers")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Customer {
 
-    @Id
-    @GeneratedValue
-    private UUID customerId;
+	@Id
+	@GeneratedValue
+	private UUID customerId;
 
-    private String businessName;
-    private String VATNumber;
-    private String email;
-    private LocalDate added;
-    private LocalDate lastContact;
-    private BigDecimal annualTurnover;
-    private String pec;
-    private String phoneNo;
-    private String contactEmail;
-    private String contactName;
-    private String contactLastname;
-    private String contactPhone;
-    private String legalAddress;
-    private String operationalAddress;
+	private String businessName;
+	private String VATNumber;
+	private String email;
+	private LocalDate added;
+	private LocalDate lastContact;
+	private BigDecimal annualTurnover;
+	private String pec;
+	private String phoneNo;
+	private String contactEmail;
+	private String contactName;
+	private String contactLastname;
+	private String contactPhone;
 
-    //@ManyToOne
-    //private List<Invoice> invoices;
+	@OneToOne
+	@JoinColumn(name = "legal_address_id")
+	private Address legalAddress;
 
-    //@ManyToOne
-    //private List<Users> users;
+	@OneToOne
+	@JoinColumn(name = "operational_address_id")
+	private Address operationalAddress;
 
-    @Enumerated(EnumType.STRING)
-    private BusinessType customerType;
+	// @ManyToOne
+	// private List<Invoice> invoices;
+
+	// @ManyToOne
+	// private List<Users> users;
+
+	@Enumerated(EnumType.STRING)
+	private BusinessType customerType;
 
 }
