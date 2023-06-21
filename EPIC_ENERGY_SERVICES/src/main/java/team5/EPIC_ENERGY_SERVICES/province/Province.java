527d64f6edbef3efb8b6e709cf1c547029220fd5
Nestor162 <nestorcicardini@gmail.com>
2023-06-21 15:21:31 +0200

    Create Province Entity

---
+++ EPIC_ENERGY_SERVICES/src/main/java/team5/EPIC_ENERGY_SERVICES/province/Province.java
@@ -0,0 +1,27 @@
+package team5.EPIC_ENERGY_SERVICES.province;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import team5.EPIC_ENERGY_SERVICES.municipality.Municipality;
+
+@Entity
+@Table(name = "Provinces")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Province {
+	@Id
+	private String initials;
+	private String name;
+	private String region;
+
+	@OneToMany(mappedBy = "province")
+	private List<Municipality> municipalities;
+}
---
+++ EPIC_ENERGY_SERVICES/src/main/java/team5/EPIC_ENERGY_SERVICES/province/ProvinceRepository.java
@@ -0,0 +1,7 @@
+package team5.EPIC_ENERGY_SERVICES.province;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProvinceRepository extends JpaRepository<Province, String> {
+
+}
