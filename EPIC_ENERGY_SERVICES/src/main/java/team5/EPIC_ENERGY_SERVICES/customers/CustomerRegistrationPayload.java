9f5cf4abe4d17263bc4464d22e7e5bf30640238f
Marianna <marianna.albrici@gmail.com>
2023-06-22 17:48:34 +0200

    Finally finished customerregistrationpayload

--- EPIC_ENERGY_SERVICES/src/main/java/team5/EPIC_ENERGY_SERVICES/customers/CustomerRegistrationPayload.java
+++ EPIC_ENERGY_SERVICES/src/main/java/team5/EPIC_ENERGY_SERVICES/customers/CustomerRegistrationPayload.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import team5.EPIC_ENERGY_SERVICES.address.Address;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -41,4 +42,8 @@ public class CustomerRegistrationPayload {
     String contactPhone;
 
     BusinessType customerType;
+
+    Address legalAddress;
+
+    Address operationalAddress;
 }
